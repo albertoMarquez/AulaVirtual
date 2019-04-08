2032c091fcaa07fc61f4bfa493c83ee4122cea54
Alberto Márquez Gómez <albmarqu@ucm.es>
2019-04-08 17:36:16 +0200

    Subir alumnos

--- public/js/subirAlumno.js
+++ public/js/subirAlumno.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {  
+$(document).ready(function() { 
     $("#desconectar").removeClass("hidden");
     $("#desconectar").click(function(event) {
         $.galleta().setc("usuario", "undefined", "Thu, 01 Jan 1970 00:00:01 GMT");
@@ -14,34 +14,94 @@ $(document).ready(function() {
         $("#solProf").attr("disabled", true);
         $("#botonPdf").click(function(event){
             event.preventDefault();
-    
+   
             var doc = $("#botonPdf").attr("href");
-    
+   
             var htmlText = '<iframe width=100% height=100%'
                          + ' type="application/pdf"'
                          + ' src="'
                          + encodeURI(doc)
-                         + '"></iframe>'; 
+                         + '"></iframe>';
             // Open PDF in new browser window
             var detailWindow = window.open ("");
             detailWindow.document.write(htmlText);
-            detailWindow.document.close(); 
+            detailWindow.document.close();
         });
         //subirAlumno
         //cargarProblemaAlumno();
         $("#evaluar").click(function(event) {
+            ///PROGRAMAR FUNCIONES
             event.preventDefault();
-            subirAlumno();
+            subirScriptlumno(user);
         });
     }else{
         var link = window.location.href;
         var res = link.split("/");
         window.location = res[1] + "/";
     }
-});
-$("#evaluar").click(function(event) {
-///PROGRAMAR FUNCIONES
-});
-function subirAlumno(){
+ });
+ 
+ function entregaRetrasada(idEjercicio){
+    let fechaSubida = new Date();
+    info = {fechaSubida: fechaSubida};
+    $.ajax({
+        method: "POST",
+        url: "/entregaRetrasada",
+        data: JSON.stringify(info),
+        dataType:"JSON",
+        contentType: "application/json",
+        success: function(retrasada) {
+            if(retrasada)
+                alert("Fecha retrasada");
+            var link = window.location.href;
+            var res = link.split("/");
+            window.location = res[1] + "/principal.html";
+        },
+        error: function() {
+            alert("Error al comprobar la fecha de entrega");
+        } 
+    });
+ }
+ function subirScriptlumno(user){
     alert("hola");
-}
+    var link = window.location.href;
+    var res = link.split("/");
+    let idEjercicio = link[link.length-1];//coger del id
+    alert(idEjercicio);
+    let solucion = $("procedimientoAlumno").val();
+    //let nota
+    //let numOk
+    //let entregaRetrasada = entregaRetrasada(idEjercicio);
+    let correccionProfesor
+    let idAlumno = user.idAlumno;
+    let idGrupo = user.idGrupo;
+    //let intentos
+    let resultado = NULL; //se tiene que coger del oracledb
+    let fechaActual = new Date();
+ 
+    const reader = new FileReader();
+    reader = $('input[type=file]')[0].files[0];
+    if(reader.EMPTY){
+        info = {scripts: archivosTo64, enun:enun64, tablas: tablas64, solucion: solucion64, titu:titulo, idProfesor:user.idProfesor};
+        $.ajax({
+            method: "POST",
+            url: "/subirEjercicio",
+            data: JSON.stringify(info),
+            dataType:"JSON",
+            contentType: "application/json",
+            success: function() {
+                alert("se ha subido correctamente el ejercicio");
+                var link = window.location.href;
+                var res = link.split("/");
+                window.location = res[1] + "/principal.html";
+            },
+            error: function() {
+                alert("Error al subir un nuevo ejercicio.");
+            } 
+        });
+    }else{
+        alert("la");
+    }
+ }
+ 
+ 
