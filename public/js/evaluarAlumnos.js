dcbb92f4a191bf15e118b8e3368904a7f7f51f7c
Marta Rodriguez <martaRodri@users.noreply.github.com>
2019-04-09 20:26:40 +0200

    evaluar alumnos

--- public/js/evaluarAlumnos.js
+++ public/js/evaluarAlumnos.js
@@ -89,15 +89,18 @@ function abrirModal(data){
    
     modal.style.display = "block";
 
+    console.log(data);
+    $("#nombreModal").text(data[0] + " " + data[1]);
+    $("#notaModal").val(data[10]);
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
+   /* window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    }
+    }*/
 }
