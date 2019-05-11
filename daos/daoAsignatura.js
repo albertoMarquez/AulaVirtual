6267357cdc053a2ffd739fd2ccad1618aa603a3c
Marta Rodriguez <martaRodri@users.noreply.github.com>
2019-05-11 19:25:21 +0200

    cambios evaluaAlumno

--- daos/daoAsignatura.js
+++ daos/daoAsignatura.js
@@ -9,12 +9,13 @@ class DAOAsignatura {
         this.pool = pool;
     }
 
-    listarAsignaturas(callback){
+    listarAsignaturas(idProfe, callback){
         this.pool.getConnection((err, con) =>{
             if(err){
                 callback(err);
             }else{
-                con.query(`SELECT * FROM asignatura`, (err, filas)=>{
+                con.query(`select * from asignatura a join grupos g join profegrupo pg on a.idAsignatura = g.idAsignatura and pg.idGrupo = g.idGrupo and pg.idProfesor = ?`,
+                [idProfe], (err, filas)=>{
                     if(err){
                         callback(err, undefined);
                     }else{
@@ -38,12 +39,15 @@ class DAOAsignatura {
         });
     }
 
-    listarCursoGrupo(id, callback){ //dada un id de asignatura lista curso y grupo
+    listarCursoGrupo(idAsignatura, idProfe, callback){ //dada un id de asignatura lista curso y grupo
+        var f = new Date();
+        var anioHoy = f.getFullYear();
         this.pool.getConnection((err, con) =>{
             if(err){
                 callback(err);
             }else{
-                con.query(`SELECT * FROM asignatura a, grupos g where a.idAsignatura = g.idAsignatura AND a.idAsignatura = ?`, [id],
+                con.query(`SELECT * FROM asignatura a, grupos g, profeGrupo pg where a.idAsignatura = g.idAsignatura and g.idGrupo = pg.idGrupo AND a.idAsignatura = 1 and anio = '2018' and pg.idProfesor = 1`,
+                 [idAsignatura, anioHoy, idProfe],
                 (err, filas) =>{
                     if(err){
                         callback(err, undefined);
