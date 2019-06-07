2346228f000bf5615d07102bccd7880d992c3864
albertoMarquez <albmarqu@ucm.es>
2019-06-07 20:09:55 +0200

    cambio en aulavirtual.sql

--- dbconfig.js
+++ dbconfig.js
@@ -6,14 +6,14 @@ module.exports = {
     // passing it in an environment variable via process.env, or using
     // External Authentication.
     //Profe
-    //password      : process.env.NODE_ORACLEDB_PASSWORD || "adbd18",
-    password      : process.env.NODE_ORACLEDB_PASSWORD || "SYS",
+    password      : process.env.NODE_ORACLEDB_PASSWORD || "adbd18",
+    //password      : process.env.NODE_ORACLEDB_PASSWORD || "SYS",
   
     // For information on connection strings see:
     // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
     //Profe
-   // connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "192.168.56.101:1521/orcl",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost",
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "192.168.56.101:1521/orcl",
+    //connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost",
   
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
